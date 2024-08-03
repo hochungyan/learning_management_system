e6114b53bba78721c3803d7c174db0cc691e4bf5
hochungyan <hochungyan@live.hk>
2024-08-03 07:16:37 +0100

    feautre-lms-07: pre-commit fix all files

--- backend/tests/admin_views_test.py
+++ backend/tests/admin_views_test.py
@@ -1,22 +1,27 @@
 import pytest
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework.test import APIClient
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
 
 @pytest.fixture
 def api_client():
     return APIClient()
 
+
 @pytest.fixture
 def admin_user():
-    return User.objects.create_superuser(email='admin@example.com', password='test')
+    return User.objects.create_superuser(
+        email="admin@example.com", password="test"
+    )
+
 
 @pytest.mark.django_db
 def test_admin_login_redirects_to_login_page(api_client):
     """
-    Tests that accessing the admin page without logging in redirects to the login page.
+    Tests admin page access without login redirects to login page.
 
     Args:
         api_client (APIClient): The Django REST framework API client.
@@ -27,8 +32,9 @@ def test_admin_login_redirects_to_login_page(api_client):
     Raises:
         AssertionError: If the response status code is not 302 (redirect).
     """
-    response = api_client.get(reverse('admin:index'))
+    response = api_client.get(reverse("admin:index"))
     assert response.status_code == 302
+
 
 @pytest.mark.django_db
 def test_admin_login_successful(api_client, admin_user):
@@ -45,8 +51,11 @@ def test_admin_login_successful(api_client, admin_user):
     Raises:
         AssertionError: If the login is not successful.
     """
-    login_successful = api_client.login(email='admin@example.com', password='test')
+    login_successful = api_client.login(
+        email="admin@example.com", password="test"
+    )
     assert login_successful
+
 
 @pytest.mark.django_db
 def test_admin_access_after_logging_in(api_client, admin_user):
@@ -64,5 +73,5 @@ def test_admin_access_after_logging_in(api_client, admin_user):
         AssertionError: If the response status code is not 200.
     """
     api_client.force_login(admin_user)
-    response = api_client.get(reverse('admin:index'))
+    response = api_client.get(reverse("admin:index"))
     assert response.status_code == 200
--- backend/userauths/apps.py
+++ backend/userauths/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class UserauthsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'userauths'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "userauths"
