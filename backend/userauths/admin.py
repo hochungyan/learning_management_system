6d9b52320f191a767ab22aae934a20d5fdb54770
hochungyan <hochungyan@live.hk>
2024-08-04 01:16:09 +0100

    bug-lms-09: migration bug fix

--- backend/userauths/admin.py
+++ backend/userauths/admin.py
@@ -1,1 +1,56 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+
+from .models import Profile, User
+
+
+# Define an inline admin descriptor for Profile model
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+    list_display = ("email", "username", "full_name", "is_staff")
+    search_fields = ("email", "username", "full_name")
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("username", "full_name", "otp")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+
+
+# Re-register UserAdmin
+admin.site.register(User, UserAdmin)
+
+# Register the Group model if it's not already registered
+try:
+    admin.site.register(Group)
+except admin.sites.AlreadyRegistered:
+    pass
