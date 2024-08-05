c415af89274c249f4049217b2ce88e78e2045ac3
Chung Yan Ho <100369959+hochungyan@users.noreply.github.com>
2024-08-05 04:43:27 +0100

    bug-lms-05:security leak (#19)
    * bug-lms-05:security leak
    
    * bug-lms-05:secret added
    
    * bug-lms-05: fixing

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,9 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # SECURITY WARNING:
 # keep the secret key used in production secret!
-SECRET_KEY = (
-    "NEW_SECRET_KEY"
-)
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
