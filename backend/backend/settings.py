f4e059488e904ece132b438ce46f5448feeab3a3
hochungyan <hochungyan@live.hk>
2024-08-05 04:27:28 +0100

    bug-lms-05:security leak

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from django.core.exceptions import ImproperlyConfigured
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -22,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # SECURITY WARNING:
 # keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-=@al_yehsmb@n#u+az^h^)!+j#=p#ulkj&pleq-%=!c8546*r1"
-)
-
+SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    raise ImproperlyConfigured(
+        "The SECRET_KEY environment variable is not set"
+    )
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
